af3188eb828652f6350953bf2e3568e524c17ecc
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 17:53:14 -0300

    add better error messages and home icon buttom

--- src/app/book/[bookId]/graph/Error.tsx
+++ src/app/book/[bookId]/graph/Error.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 
-export const Error = () => {
+export const Error = ({message}: {message: string}) => {
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col gap-4">
-      <h1 className="text-destructive text-4xl">Error creating graph</h1>
+      <h1 className="text-destructive text-4xl">{message}</h1>
       <Link href="/">
         <Button>Try again</Button>
       </Link>
