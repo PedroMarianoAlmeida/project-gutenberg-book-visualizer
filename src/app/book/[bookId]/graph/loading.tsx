f7f010446b5ab770c78cbf62e91434e86dc2f3f2
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 16:55:42 -0300

    Add loading page to graph

---
+++ src/app/book/[bookId]/graph/loading.tsx
@@ -0,0 +1,21 @@
+"use client";
+import RotatingText from "@/components/react-bits/RotatingText";
+
+export const GraphLoading = () => {
+  return (
+    <RotatingText
+      texts={["Generating Graph", "This can take a while"]}
+      mainClassName="px-2 sm:px-2 md:px-3 bg-[#aa873b] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg w-72"
+      staggerFrom={"last"}
+      initial={{ y: "100%" }}
+      animate={{ y: 0 }}
+      exit={{ y: "-120%" }}
+      staggerDuration={0.025}
+      splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+      transition={{ type: "spring", damping: 30, stiffness: 400 }}
+      rotationInterval={2000}
+    />
+  );
+};
+
+export default GraphLoading;
