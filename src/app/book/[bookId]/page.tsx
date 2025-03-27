3e2135e1e5249c79603cacde60c8ceb858317055
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 13:27:37 -0300

    connect with dynamic route

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -1,6 +1,6 @@
-// import { getBookText } from "@/services/gutenbergService";
-// import { createGraphData } from "@/services/aiServices";
-import mockData from "@/app/book/[bookId]/graphDataSample.json"
+import { getBookText } from "@/services/gutenbergService";
+import { createGraphData } from "@/services/aiServices";
+import mockData from "@/app/book/[bookId]/graphDataSample.json";
 import { Result } from "./Result";
 
 export const BookPage = async ({
@@ -9,26 +9,27 @@ export const BookPage = async ({
   params: Promise<{ bookId: string }>;
 }) => {
   const { bookId } = await params;
-//   const bookText = await getBookText(Number(bookId));
+  const bookText = await getBookText(Number(bookId));
 
-//   if (!bookText.success) {
-//     return (
-//       <p className="text-destructive">
-//         {bookText.message === "Book not found"
-//           ? "Book not found"
-//           : "Error fetching data"}
-//       </p>
-//     );
-//   }
+  if (!bookText.success) {
+    return (
+      <p className="text-destructive">
+        {bookText.message === "Book not found"
+          ? "Book not found"
+          : "Error fetching data"}
+      </p>
+    );
+  }
 
-//   const graphData = await createGraphData(bookText.result);
-//   if (!graphData.success)
-//     return <p className="text-destructive">Error creating graph</p>;
+  const graphData = await createGraphData(bookText.result);
+  if (!graphData.success)
+    return <p className="text-destructive">Error creating graph</p>;
 
   return (
     <div>
       <h1>Book {bookId} Page</h1>
-     <Result graphData={mockData}/>
+      <Result graphData={graphData.result.bokGraphData} />
+      {/* <Result graphData={mockData}/> */}
     </div>
   );
 };
