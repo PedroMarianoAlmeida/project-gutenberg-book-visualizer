8afd87e3e7de13f35250bf3193d57102497cca71
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 18:26:14 -0300

    Improve graph page (#8)
    * add better error messages and home icon buttom
    
    * add canvas full screen

--- src/app/book/[bookId]/graph/page.tsx
+++ src/app/book/[bookId]/graph/page.tsx
@@ -1,4 +1,7 @@
-import { getBookText } from "@/services/gutenbergService";
+import Link from "next/link";
+import { Home } from "lucide-react";
+
+import { getBookText, getBookMetadata } from "@/services/gutenbergService";
 import { createGraphData } from "@/services/aiServices";
 import mockData from "./graphDataSample.json";
 import { Result } from "./Result";
@@ -11,26 +14,49 @@ export const BookPage = async ({
 }) => {
   const { bookId } = await params;
   const bookText = await getBookText(Number(bookId));
+  const bookMetadata = await getBookMetadata(Number(bookId));
+
+  if (!bookMetadata.success)
+    return (
+      <Error
+        message={
+          bookMetadata.message === "Book not found"
+            ? "Book not found"
+            : "Error fetching book content"
+        }
+      />
+    );
 
   if (!bookText.success) {
     return (
-      <p className="text-destructive">
-        {bookText.message === "Book not found"
-          ? "Book not found"
-          : "Error fetching data"}
-      </p>
+      <Error
+        message={
+          bookText.message === "Book not found"
+            ? "Book not found"
+            : "Error fetching book content"
+        }
+      />
     );
   }
 
   const graphData = await createGraphData(bookText.result);
-  if (!graphData.success) return <Error />;
+  if (!graphData.success) return <Error message="Error creating graph" />;
 
   return (
-    <div>
-      <h1>Book {bookId} Page</h1>
+    <main className="p-4 w-full">
+      <h1 className="text-center text-xl w-full">
+        {bookMetadata.result.Title}
+      </h1>
+      <Link
+        href="/"
+        className="absolute bottom-4 left-4 p-2 rounded-full bg-background/80 backdrop-blur-sm shadow-sm hover:bg-background transition-colors duration-200"
+        aria-label="Go back to home page"
+      >
+        <Home className="h-5 w-5" />
+      </Link>
       <Result graphData={graphData.result.bokGraphData} />
-      {/* <Result graphData={mockData}/> */}
-    </div>
+      {/* <Result graphData={mockData} /> */}
+    </main>
   );
 };
 
