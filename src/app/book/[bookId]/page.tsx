e78902f1eed2f9f57e92371507465ee44dcc73ae
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 12:00:42 -0300

    reading dynamic route

--- src/app/book/[bookId]/page.tsx
+++ src/app/book/[bookId]/page.tsx
@@ -1,5 +1,10 @@
-export const BookPage = () => {
-  return <h1>Book Page</h1>;
+export const BookPage = async ({
+  params,
+}: {
+  params: Promise<{ bookId: string }>;
+}) => {
+  const { bookId } = await params;
+  return <h1>Book {bookId} Page</h1>;
 };
 
 export default BookPage;
