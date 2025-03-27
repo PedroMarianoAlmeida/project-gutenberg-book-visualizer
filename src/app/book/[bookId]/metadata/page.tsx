313be3eb37921d9bcf4be37a1275f3404813ee5d
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 13:58:55 -0300

    getBookMetadata from url
    But the page is asking to not scrap there and offer alternatives

--- src/app/book/[bookId]/metadata/page.tsx
+++ src/app/book/[bookId]/metadata/page.tsx
@@ -1,10 +1,13 @@
+import { getBookMetadata } from "@/services/gutenbergService";
+
 export const BookPage = async ({
   params,
 }: {
   params: Promise<{ bookId: string }>;
 }) => {
   const { bookId } = await params;
-
+  const bookMetadata = await getBookMetadata(Number(bookId));
+  console.log({ bookMetadata });
   return (
     <div>
       <h1>Book {bookId} Page</h1>
--- src/services/gutenbergService.ts
+++ src/services/gutenbergService.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import { asyncWrapper } from "@/utils/asyncWrapper";
 import { streamToString } from "@/utils/streamToString";
 
@@ -5,9 +7,19 @@ const gutenbergUrl = "https://www.gutenberg.org/";
 
 export const getBookText = async (bookId: number) => {
   return asyncWrapper(async () => {
-    const res = await fetch(
-      `${gutenbergUrl}files/${bookId}/${bookId}-0.txt`
-    );
+    const res = await fetch(`${gutenbergUrl}files/${bookId}/${bookId}-0.txt`);
+    if (res.status === 404) throw new Error("Book not found");
+    if (!res.ok) throw new Error("Error fetching book");
+    if (res.body === null) throw new Error("Book has no content");
+
+    const bookText = await streamToString(res.body);
+    return bookText;
+  });
+};
+
+export const getBookMetadata = async (bookId: number) => {
+  return asyncWrapper(async () => {
+    const res = await fetch(`${gutenbergUrl}ebooks/${bookId}`);
     if (res.status === 404) throw new Error("Book not found");
     if (!res.ok) throw new Error("Error fetching book");
     if (res.body === null) throw new Error("Book has no content");
