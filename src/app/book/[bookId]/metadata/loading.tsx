596c98964567381fdbc438558379042be3102524
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-27 19:45:28 -0300

    add load and remove animation from step2

--- src/app/book/[bookId]/metadata/loading.tsx
+++ src/app/book/[bookId]/metadata/loading.tsx
@@ -1,32 +1,30 @@
 "use client";
-import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
-import Stepper, { Step } from "@/components/react-bits/Stepper";
+import Stepper, { StepperHandle, Step } from "@/components/react-bits/Stepper";
 
 import { Button } from "@/components/ui/button";
 
 import { BookForm } from "@/components/StaticBookForm";
 
-interface MetaDataClientProps {
-  bookId: number;
-  bookTitle: string;
-  bookAuthor: string;
-}
-export const MetaDataClient = ({
-  bookAuthor,
-  bookId,
-  bookTitle,
-}: MetaDataClientProps) => {
+export default function Loading() {
+  const stepperRef = useRef<StepperHandle>(null);
+
+  useEffect(() => {
+    if (stepperRef.current) {
+      stepperRef.current.nextStep();
+    }
+  }, []);
+
   return (
     <main>
-      <Stepper initialStep={2} disableStepIndicators>
+      <Stepper initialStep={1} disableStepIndicators ref={stepperRef}>
         <Step>
-          <></>
+          <BookForm bookId={"???"} />
         </Step>
         <Step>
           <div className="flex flex-col gap-4 items-center">
-            <h1 className="text-center">Book #{bookId}</h1>
+            <h1 className="text-center">Book #XXX</h1>
             <h2 className="text-center text-xl">
               It is this book that you are looking for? 🤓
             </h2>
@@ -34,23 +32,24 @@ export const MetaDataClient = ({
               <li className="mb-4">
                 <div className="flex flex-col items-center">
                   <span className="font-bold">Title</span>
-                  <span className="text-center">{bookTitle}</span>
+                  <span className="text-center">Skeleton 1</span>
                 </div>
               </li>
               <li>
                 <div className="flex flex-col items-center">
                   <span className="font-bold">Author(s)</span>
-                  <span className="text-center">{bookAuthor}</span>
+                  <span className="text-center">Skeleton 2</span>
                 </div>
               </li>
             </ul>
             <div className="flex flex-col gap-4">
-              <Link href={`/book/${bookId}/graph`}>
-                <Button variant="outline">Yes, create my Graph</Button>
-              </Link>
-              <Link href="/">
-                <Button variant="destructive">No, I will add another Id</Button>
-              </Link>
+              <Button variant="outline" disabled>
+                Yes, create my Graph
+              </Button>
+
+              <Button variant="destructive" disabled>
+                No, I will add another Id
+              </Button>
             </div>
           </div>
         </Step>
@@ -60,4 +59,4 @@ export const MetaDataClient = ({
       </Stepper>
     </main>
   );
-};
+}
