4acb8fa9b63b1b89665ac18a1fd4d339e4d634f5
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-28 12:55:48 -0300

    request graph from db and all rest still working

--- src/app/book/[bookId]/graph/page.tsx
+++ src/app/book/[bookId]/graph/page.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 import { Home } from "lucide-react";
 
 import { getBookText, getBookMetadata } from "@/services/gutenbergService";
-import { createGraphData } from "@/services/aiServices";
-// import mockData from "./graphDataSample.json";
+import { createGraphData } from "@/services/aiService";
+import { getGraphFromDatabase } from "@/services/dbService";
+
+import mockData from "./graphDataSample.json";
 import { Result } from "./Result";
 import { Error } from "./Error";
 import { ChatCTA } from "./ChatCTA";
@@ -16,28 +18,25 @@ export default async function Page({
   params: Promise<{ bookId: string }>;
 }) {
   const { bookId } = await params;
+  const bookIdNumber = Number(bookId);
 
-  const [bookText, bookMetadata] = await Promise.all([
-    getBookText(Number(bookId)),
-    getBookMetadata(Number(bookId)),
-  ]);
+  const [bookTextResult, bookMetadataResult, graphFromDbResult] =
+    await Promise.allSettled([
+      getBookText(bookIdNumber),
+      getBookMetadata(bookIdNumber),
+      getGraphFromDatabase(bookIdNumber),
+    ]);
 
-  if (!bookMetadata.success)
+  if (
+    bookMetadataResult.status !== "fulfilled" ||
+    !bookMetadataResult.value.success
+  ) {
     return (
       <Error
         message={
-          bookMetadata.message === "Book not found"
-            ? "Book not found"
-            : "Error fetching book content"
-        }
-      />
-    );
-
-  if (!bookText.success) {
-    return (
-      <Error
-        message={
-          bookText.message === "Book not found"
+          bookMetadataResult.status === "fulfilled" &&
+          !bookMetadataResult.value.success &&
+          bookMetadataResult.value.message === "Book not found"
             ? "Book not found"
             : "Error fetching book content"
         }
@@ -45,14 +44,39 @@ export default async function Page({
     );
   }
 
-  const graphData = await createGraphData(bookText.result);
-  if (!graphData.success) return <Error message="Error creating graph" />;
+  if (bookTextResult.status !== "fulfilled" || !bookTextResult.value.success) {
+    return (
+      <Error
+        message={
+          bookTextResult.status === "fulfilled" &&
+          !bookTextResult.value.success &&
+          bookTextResult.value.message === "Book not found"
+            ? "Book not found"
+            : "Error fetching book content"
+        }
+      />
+    );
+  }
+
+  const bookTitle = bookMetadataResult.value.result.Title;
+
+  let graphData;
+  if (
+    graphFromDbResult.status === "fulfilled" &&
+    graphFromDbResult.value.success
+  ) {
+    graphData = graphFromDbResult.value;
+    console.log({ graphData });
+  } else {
+    graphData = await createGraphData(bookTextResult.value.result);
+    if (!graphData.success) return <Error message="Error creating graph" />;
+  }
+
+  console.log({ bookText: bookTextResult.value.result });
 
   return (
     <main className="p-4 w-full">
-      <h1 className="text-center text-xl w-full">
-        {bookMetadata.result.Title}
-      </h1>
+      <h1 className="text-center text-xl w-full">{bookTitle}</h1>
       <Link
         href="/"
         className="absolute bottom-4 left-4 p-2 rounded-full bg-background/80 backdrop-blur-sm shadow-sm hover:bg-background transition-colors duration-200 z-10"
@@ -60,9 +84,8 @@ export default async function Page({
       >
         <Home className="h-5 w-5" />
       </Link>
-      <Result graphData={graphData.result.bokGraphData} />
+      {/* <Result graphData={graphData.result.bokGraphData} /> */}
       <ChatCTA bookId={bookId} />
-      {/* <Result graphData={mockData} /> */}
     </main>
   );
 }
--- src/services/dbService.ts
+++ src/services/dbService.ts
@@ -1,7 +1,8 @@
 import { doc, getDoc } from "firebase/firestore";
-import { database } from "@/config/databaseConfig";
 
+import { database } from "@/config/databaseConfig";
 import { asyncWrapper } from "@/utils/asyncWrapper";
+import { graphAiSchema } from "@/services/aiService";
 
 // https://firebase.google.com/docs/firestore/query-data/get-data
 
@@ -22,5 +23,17 @@ export const getBookIdsAlreadyRegistered = () => {
     const ids = data.ids.filter((id): id is string => typeof id === "string");
 
     return { ids };
+  });
+};
+
+export const getGraphFromDatabase = (bookId: number) => {
+  return asyncWrapper(async () => {
+    const docRef = doc(database, "graphs", String(bookId));
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists) throw new Error("Document not founded");
+
+    const data = docSnap.data();
+    console.log({ data });
+    return { data };
   });
 };
