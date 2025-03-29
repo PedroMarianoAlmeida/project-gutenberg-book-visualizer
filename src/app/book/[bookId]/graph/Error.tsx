ba42a6587eff641912afd3bad6131bd7684de4f3
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-29 02:14:00 -0300

    remove-nodes-without-edged

--- src/app/book/[bookId]/graph/Error.tsx
+++ src/app/book/[bookId]/graph/Error.tsx
@@ -1,13 +1,20 @@
+"use client";
+import { useState } from "react";
 import Link from "next/link";
+import { Loader2 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
-export const Error = ({message}: {message: string}) => {
+export const Error = ({ message }: { message: string }) => {
+  const [loading, setLoading] = useState(false);
+
   return (
     <main className="w-full h-screen flex justify-center items-center flex-col gap-4">
       <h1 className="text-destructive text-4xl">{message}</h1>
       <Link href="/">
-        <Button>Try again</Button>
+        <Button onClick={() => setLoading(true)}>
+          Try again {loading && <Loader2 className="animate-spin" />}
+        </Button>
       </Link>
     </main>
   );
