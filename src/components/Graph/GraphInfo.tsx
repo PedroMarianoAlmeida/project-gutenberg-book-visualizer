cdc79b9813cb77681e1e1954f32d6d15d2e90028
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-29 01:01:24 -0300

    graph info with scroll

--- src/components/Graph/GraphInfo.tsx
+++ src/components/Graph/GraphInfo.tsx
@@ -14,6 +14,7 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { EnhancedGraphData } from "@/utils/graphDataSanitize";
 import { useDarkMode } from "@/hooks/useDarkTheme";
@@ -33,26 +34,28 @@ export const GraphInfo = ({
         <SheetHeader>
           <SheetTitle>Graph's Info</SheetTitle>
           <SheetDescription>
-            <Accordion type="single" collapsible className="w-full">
-              {enhancedGraphData.nodes.map((node) => (
-                <AccordionItem value={node.id} key={node.id}>
-                  <AccordionTrigger key={node.id}>
-                    <div className="flex gap-2 items-center">
-                      <div
-                        className="h-4 w-4 rounded-full"
-                        style={{
-                          backgroundColor: isDark
-                            ? node.color.dark
-                            : node.color.light,
-                        }}
-                      />
-                      <p>{node.id}</p>
-                    </div>
-                  </AccordionTrigger>
-                  <AccordionContent>{node.description}</AccordionContent>
-                </AccordionItem>
-              ))}
-            </Accordion>
+            <ScrollArea className="h-screen">
+              <Accordion type="single" collapsible className="w-full px-3">
+                {enhancedGraphData.nodes.map((node) => (
+                  <AccordionItem value={node.id} key={node.id}>
+                    <AccordionTrigger key={node.id}>
+                      <div className="flex gap-2 items-center">
+                        <div
+                          className="h-4 w-4 rounded-full"
+                          style={{
+                            backgroundColor: isDark
+                              ? node.color.dark
+                              : node.color.light,
+                          }}
+                        />
+                        <p>{node.id}</p>
+                      </div>
+                    </AccordionTrigger>
+                    <AccordionContent>{node.description}</AccordionContent>
+                  </AccordionItem>
+                ))}
+              </Accordion>
+            </ScrollArea>
           </SheetDescription>
         </SheetHeader>
       </SheetContent>
