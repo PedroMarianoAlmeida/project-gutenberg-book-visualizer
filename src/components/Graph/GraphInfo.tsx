06fe9e3319bb763c72cdf5489217528a8bad2e31
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2025-03-29 00:36:15 -0300

    fix theme color

--- src/components/Graph/GraphInfo.tsx
+++ src/components/Graph/GraphInfo.tsx
@@ -10,12 +10,14 @@ import {
 } from "@/components/ui/sheet";
 
 import { EnhancedGraphData } from "@/utils/graphDataSanitize";
+import { useDarkMode } from "@/hooks/useDarkTheme";
 
 export const GraphInfo = ({
   enhancedGraphData,
 }: {
   enhancedGraphData: EnhancedGraphData;
 }) => {
+  const isDark = useDarkMode();
   return (
     <Sheet>
       <SheetTrigger>
@@ -27,10 +29,14 @@ export const GraphInfo = ({
           <SheetDescription>
             <div className="flex flex-col gap-2 mt-2">
               {enhancedGraphData.nodes.map((node) => (
-                <div key={node.id} className="flex gap-2">
+                <div key={node.id} className="flex gap-2 items-center">
                   <div
                     className="h-4 w-4 rounded-full"
-                    style={{ backgroundColor: node.color.dark }}
+                    style={{
+                      backgroundColor: isDark
+                        ? node.color.dark
+                        : node.color.light,
+                    }}
                   />
                   <p>{node.id}</p>
                 </div>
